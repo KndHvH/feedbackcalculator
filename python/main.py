6e129b68cfeecb88726f14cf2d10e1cd2b13d9e4
KndHvH <delend89@gmail.com>
2022-08-04 16:31:52 -0300

    final python version

--- python/main.py
+++ python/main.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import json
-# import re
-# from pyngrok import ngrok
+import pandas as pd
+from unidecode import unidecode
 from api import *
 from match import *
 from simpleResult import *
+from listSplit import *
 
 #json api import
 goodList = apiGood()
@@ -12,33 +13,27 @@ badList = apiBad()
 
 
 # test.json file import (test only)
-
-userFeed = json.load(open("test.json"))
-# print(userFeed['feed'])
-userFeed = userFeed['feed']
 feedList=[]
-for i in userFeed:
-    feedList.append(i['feed'])
+df = pd.read_csv("dataTest.csv",sep=';')
+list=df.values.tolist()
 
+for i in range(len(list)):
+    feedList.append(unidecode(list[i][0]))
 
 results={}
+
 i=1
 #score generator
 for feed in feedList:
     goodScore = getGoodMatch(feed,goodList)
     badScore = getBadMatch(feed,badList)
     finalScore = goodScore[0] - badScore[0]
-
     results[i]=[feed,finalScore]
     i=i+1
 
-    #test metrics
-    # print(feed)
-    # print(goodScore)
-    # print(badScore)
-    # print(finalScore)
-
 results = getSimpleResult(results)
-resultsJson = json.dumps(results)
-print(resultsJson)
-print(resultsJson)
+data = listSplit(results)
+
+
+df = pd.DataFrame({"Feedback": feedList, "Score":data[0],"Result":data[1]})
+df.to_excel("results.xlsx")
